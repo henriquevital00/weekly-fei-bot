67f0cd357249efd58834ad2eb9f1148385c3086f
henriquevital00 <henriquevital00@gmail.com>
2021-02-19 19:53:27 -0300

    Updated script to insert feedback text and click in send button

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from datetime import date
 import locale
 import json
+import time
 
 
 class Bot(object):
@@ -17,6 +18,11 @@ class Bot(object):
         ]
         locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
+    def get_text_stars_json(self, subject):
+        with open('./user_config.json') as json_file:
+            obj = json.load(json_file)
+            return obj['subjects'][subject]['text']
+
     def load_subjects(self):
         week_day = date.today().weekday()
         number_of_subjects = 1
@@ -28,12 +34,21 @@ class Bot(object):
             count += len(
                 test.find_elements_by_xpath(".//ul[starts-with(@id,'aula-')]"))
             while number_of_subjects <= count:
-                print("Number: " + str(number_of_subjects))
-                self.subjects.update([
+                subject = self.driver.find_element_by_id(
+                    "aula-" + str(number_of_subjects)).text.split("-")[0]
+                self.subjects.update([subject])
+                text = self.get_text_stars_json(subject)
+                try:
                     self.driver.find_element_by_id(
-                        "aula-" + str(number_of_subjects)).text.split("-")[0]
-                ])
-                number_of_subjects += 1
+                        'consideracao-' +
+                        str(number_of_subjects)).send_keys(text)
+                    self.driver.find_element_by_id(
+                        "cadastrar-" + str(number_of_subjects)).click()
+                    time.sleep(3)
+                except:
+                    print('Element not found')
+                finally:
+                    number_of_subjects += 1
 
     def load_config(self):
         try:
@@ -46,7 +61,7 @@ class Bot(object):
 
     def login(self):
         self.driver.get(
-            "https://interage.fei.org.br/secureserver/portal/graduacao/secretaria/temporarios-e-sazonais/avaliacoes-semanais"
+            "https://interage.fei.org.br/secureserver/portal/graduacao/sala-dos-professores/aulas/presenca"
         )
         self.driver.find_element_by_id("Usuario").send_keys(self.username)
         self.driver.find_element_by_id("Senha").send_keys(self.password)
--- main.py
+++ main.py
@@ -2,18 +2,12 @@ from bot import Bot
 import sys
 
 
-def main(argv):
-    if argv[0] == "run":
-        bot = Bot()
-        #bot.load_config()
-        bot.load_config()
-        bot.login()
-        bot.load_subjects()
-    #elif sys.args[1] == "register":
-    #    bot.create_config()
-    else:
-        print("Choose one option:" "1: Registrer a new account" "2: run")
+def main():
+    bot = Bot()
+    bot.load_config()
+    bot.login()
+    bot.load_subjects()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
