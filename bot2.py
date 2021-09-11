989032ee2499869d9d6d5c56e1eb42bb0c88bb98
henriquevital00 <henriquevital1000@hotmail.com>
2021-09-11 11:33:34 -0300

    Created selenium in heroku

--- bot2.py
+++ bot2.py
@@ -5,19 +5,29 @@ import locale
 import json
 import glob
 import time
-import os
 
 
 class Bot(object):
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(
-            executable_path=os.environ.get('CHROMEDRIVER_PATH'),
-            chrome_options=chrome_options)
+        chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        #chrome_options.add_argument('--window-size=1280x1696')
+        #chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+        #chrome_options.add_argument('--hide-scrollbars')
+        #chrome_options.add_argument('--enable-logging')
+        #chrome_options.add_argument('--log-level=0')
+        #chrome_options.add_argument('--v=99')
+        #chrome_options.add_argument('--single-process')
+        #chrome_options.add_argument('--data-path=/tmp/data-path')
+        #chrome_options.add_argument('--ignore-certificate-errors')
+        #chrome_options.add_argument('--homedir=/tmp')
+        #chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+        #chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+        self.driver = webdriver.Chrome(executable_path=str(
+            glob.glob("./chromedriver*")[0]),
+                                       chrome_options=chrome_options)
         self.username = 'unifhcarvalho'
         self.password = 'hc65013'
         self.subjects = set()
